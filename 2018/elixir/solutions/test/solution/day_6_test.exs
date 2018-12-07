593d9042376f9a95f464b06b54c2d504f69fdeb5
Florian Kempenich <Flori@nKempenich.com>
2018-12-07 18:47:49 +0000

    Prepare for implementation of Day6 in Elixir

---
+++ 2018/elixir/solutions/test/solution/day_6_test.exs
@@ -0,0 +1,36 @@
+defmodule Solution.Day6Test do
+  use ExUnit.Case, async: false
+  alias Solution.Day6
+
+  @moduletag :capture_log
+
+  describe "Part 1" do
+    @tag :skip
+    test "Example from Problem Statement" do
+      assert "17" ==
+               Day6.solve_part_1("""
+               1, 1
+               1, 6
+               8, 3
+               3, 4
+               5, 5
+               8, 9
+               """)
+    end
+  end
+
+  describe "Part 2" do
+    @tag :skip
+    test "Example from Problem Statement" do
+      assert "?????" ==
+               Day6.solve_part_1("""
+               1, 1
+               1, 6
+               8, 3
+               3, 4
+               5, 5
+               8, 9
+               """)
+    end
+  end
+end
