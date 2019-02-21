6127a0b6455897e9d05b4c9e8b096f5639adc598
Florian Kempenich <Flori@nKempenich.com>
2019-02-21 13:44:46 +0000

    Automatically generate colors

--- 2018/extra/vizualisation/sandbox.py
+++ 2018/extra/vizualisation/sandbox.py
@@ -6,6 +6,7 @@ import matplotlib
 from typing import NamedTuple
 import math
 import requests
+from datetime import datetime
 
 
 # BASE_URL = "http://127.0.0.1:5000"
@@ -43,19 +44,41 @@ class MockMatrixServer(MatrixServer):
         return self.generator.reset_matrix()
 
 
-COLOR_MAPPING = {
+def random_rgb_color():
+    import random
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+
+def darken_color(color, amount=1.5):
+    import matplotlib.colors as mc
+    import colorsys
+    assert amount >= 1
+
+    c = colorsys.rgb_to_hls(*color)
+    r, g, b = colorsys.hls_to_rgb(c[0], c[1] / amount, c[2])
+    return int(r), int(g), int(b)
+
+
+color = random_rgb_color()
+print(color)
+darkened = darken_color(color)
+print(darkened)
+
+c1 = random_rgb_color()
+c2 = random_rgb_color()
+c3 = random_rgb_color()
+
+
+color_mapping = {
     ' ': [255, 255, 255],
-    '1': [230, 181, 255],
-    '1c': [183, 40, 255],
-
-    '2': [230, 181, 255],
-    '2c': [183, 40, 255],
-
-    '3': [230, 181, 255],
-    '3c': [183, 40, 255],
-
     '.': [0, 0, 0]
 }
+max_number_of_areas = 100
+for i in range(max_number_of_areas):
+    area_color = random_rgb_color()
+    color_mapping[f'{i+1}'] = area_color
+    color_mapping[f'{i+1}c'] = darken_color(area_color)
+COLOR_MAPPING = color_mapping
 
 
 class MatrixGifGenerator:
@@ -103,12 +126,21 @@ class MatrixGifGenerator:
 
     def _init_animation(self, number_of_frames, update_interval):
         def update(frame) -> None:
+            frame_start_time = datetime.now()
+            print('')
+            print(f'Frame: {frame}')
             if frame == 0:
                 self._matrix_server.reset_matrix()
 
+            next_matrix = self._matrix_server.get_next_matrix()
+            # from pprint import pprint
+            # pprint(next_matrix)
             self._displayed_matrix.set_data(
-                self._map_to_color_matrix(
-                    self._matrix_server.get_next_matrix()))
+                self._map_to_color_matrix(next_matrix))
+
+            frame_end_time = datetime.now()
+            time_to_process = frame_end_time - frame_start_time
+            print(f'Time to process: {time_to_process}')
 
         self._animation = animation.FuncAnimation(self._figure,
                                                   update,
@@ -125,4 +157,5 @@ class MatrixGifGenerator:
 
 if __name__ == "__main__":
     gif_generator = MatrixGifGenerator(HttpMatrixServer())
-    gif_generator.generate_gif('result.gif')
+    gif_generator.generate_gif(
+        'result.gif', number_of_frames=100, update_interval=60)
