c39eaa4a7e052e4011f617af4eb4bcf41b36df81
Florian Kempenich <Flori@nKempenich.com>
2019-12-13 21:08:13 +0000

    Allow to run intcode from file

--- 2019/python/run_intcode.py
+++ 2019/python/run_intcode.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+from os import path
 
 from computer import Program
 
@@ -8,9 +9,29 @@ from computer import Program
 HARD_CODED_INTCODE = '3,11,3,12,1,11,12,13,4,13,99'
 
 
+def intcode_passed_as_argv():
+    return len(sys.argv) >= 2
+
+
+def get_from_argv():
+    def is_file():
+        return path.exists(intcode_arg)
+
+    def read_from_file():
+        intcode_file_path = intcode_arg
+        with open(intcode_file_path) as f:
+            return f.readline()
+
+    intcode_arg = sys.argv[1]
+    if is_file():
+        return read_from_file()
+    else:
+        return intcode_arg
+
+
 def get_intcode():
-    if len(sys.argv) >= 2:
-        intcode_str = sys.argv[1]
+    if intcode_passed_as_argv():
+        intcode_str = get_from_argv()
     else:
         intcode_str = HARD_CODED_INTCODE
 
