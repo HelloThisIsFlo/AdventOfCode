81e6b17f93e9a60b0eba99f0137e90407b5e0892
Florian Kempenich <Flori@nKempenich.com>
2019-12-14 15:21:54 +0000

    Minor cleanup

--- 2019/python/computer/program.py
+++ 2019/python/computer/program.py
@@ -42,9 +42,6 @@ class Runtime:
         return self._hardcoded_input_gen.__next__()
 
 
-3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
-
-
 class Program:
     def __init__(self, memory, noun=None, verb=None):
         self.runtime = Runtime(memory)
