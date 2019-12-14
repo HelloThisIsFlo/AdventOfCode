dd39ca29bc6c97dc939d9e6965bb2075598e6b33
Florian Kempenich <Flori@nKempenich.com>
2019-12-14 21:06:07 +0000

    Fix Day5

--- 2019/python/day_5.py
+++ 2019/python/day_5.py
@@ -5,7 +5,7 @@ from computer import Program, UnableToSolve
 class Day5(IntcodeDay):
     def run_program_from_input(self):
         program = Program(self.intcode)
-        program.run()
+        program.run(interactive_mode=True)
 
     def solve_part_1(self):
         self.run_program_from_input()
