2e385b9040d9b694ede50a15caf8a9513730c416
Florian Kempenich <Flori@nKempenich.com>
2020-12-08 12:13:04 +0000

    Solve Day1.1

--- 2020/java/adventofcode/src/main/java/adventofcode/Day1.java
+++ 2020/java/adventofcode/src/main/java/adventofcode/Day1.java
@@ -1,8 +1,74 @@
 package adventofcode;
 
-public class Day1 {
+import java.util.ArrayList;
+import java.util.List;
 
-  public String solvePart1(String s) {
-    throw new RuntimeException("Not Yet Implemented");
+public class Day1 extends Day {
+
+  private List<Integer> inputNumbers;
+
+  public Day1() {
+    super("day1.txt");
+  }
+
+  @Override
+  public String solvePart1() {
+    parseInputNumbers();
+
+    TwoNumbers numbersThatSumTo2020 = findTwoNumbersThatSumTo(2020, inputNumbers);
+
+    Integer productOfBothNumbers = numbersThatSumTo2020.number1 * numbersThatSumTo2020.number2;
+    return productOfBothNumbers.toString();
+  }
+
+  private void parseInputNumbers() {
+    inputNumbers = parseInput(Integer::parseInt);
+  }
+
+  @Override
+  public String solvePart2() {
+    return null;
+  }
+
+  public TwoNumbers findTwoNumbersThatSumTo(int expectedSum, List<Integer> numbers) {
+    if (numbers.size() <= 1) {
+      throw new IllegalStateException(
+          "Could not find two numbers which sum would be: " + expectedSum);
+    }
+
+    Integer candidate = numbers.get(0);
+    List<Integer> rest = new ArrayList<>(numbers);
+    rest.remove(0);
+
+    for (Integer otherNumber : rest) {
+      if (candidate + otherNumber == expectedSum) {
+        return new TwoNumbers(candidate, otherNumber);
+      }
+    }
+
+    return findTwoNumbersThatSumTo(expectedSum, rest);
+  }
+
+  static class TwoNumbers {
+
+    public final Integer number1;
+    public final Integer number2;
+
+    public TwoNumbers(Integer number1, Integer number2) {
+      this.number1 = number1;
+      this.number2 = number2;
+    }
+
+    public boolean contains(Integer number) {
+      return number.equals(number1) || number.equals(number2);
+    }
+
+    @Override
+    public String toString() {
+      return "TwoNumbers{" +
+          "number1=" + number1 +
+          ", number2=" + number2 +
+          '}';
+    }
   }
 }
--- 2020/java/adventofcode/src/test/java/adventofcode/Day1Test.java
+++ 2020/java/adventofcode/src/test/java/adventofcode/Day1Test.java
@@ -1,26 +1,66 @@
 package adventofcode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import adventofcode.Day1.TwoNumbers;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class Day1Test {
+
+  private Day1 day1;
+
+  @BeforeEach
+  void setUp() {
+    day1 = new Day1();
+  }
+
+  @Nested
+  class Find2NumberThatGiveASum {
+
+    @Test
+    void emptyList_throwError() {
+      assertThrows(IllegalStateException.class, () -> {
+        day1.findTwoNumbersThatSumTo(2020, Collections.emptyList());
+      });
+    }
+
+    @Test
+    void listWithTwoElements_valid_returnResult() {
+      TwoNumbers result = day1.findTwoNumbersThatSumTo(2020, List.of(1000, 1020));
+
+      assertTrue(result.contains(1000));
+      assertTrue(result.contains(1020));
+    }
+
+    @Test
+    void listWithTwoElements_invalid_throwError() {
+      assertThrows(IllegalStateException.class, () -> {
+        day1.findTwoNumbersThatSumTo(2020, List.of(1, 2));
+      });
+    }
+  }
 
   @Nested
   class Integration {
 
     @Test
     void exampleFromTheProblemStatement() {
-      Day1 day1 = new Day1();
-      String result = day1.solvePart1(
-          "1721\n"
-              + "979\n"
-              + "366\n"
-              + "299\n"
-              + "675\n"
-              + "1456"
+      day1.inputLines = List.of(
+          "1721",
+          "979",
+          "366",
+          "299",
+          "675",
+          "1456"
       );
+
+      String result = day1.solvePart1();
 
       assertEquals("514579", result);
     }
